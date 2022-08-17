8b0320d7b63f25c7b70af022791408e243bf604a
mox727forever <opolskiji10@gmail.com>
2022-08-17 21:43:17 +0300

    fix sender_chat is None error

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ dp = aiogram.Dispatcher(bot)
 
 @dp.message_handler(content_types=aiogram.types.ContentType.all())
 async def new_post(msg: aiogram.types.Message):
-    if msg.sender_chat.type == "channel":
-        for i in msg.entities + msg.caption_entities:
-            if i.type == "custom_emoji":
-                await repost_message(msg)
-                return
+    if msg.sender_chat is not None:
+        if msg.sender_chat.type == "channel":
+            for i in msg.entities + msg.caption_entities:
+                if i.type == "custom_emoji":
+                    await repost_message(msg)
+                    return
 
 
 async def repost_message(msg: aiogram.types.Message):
